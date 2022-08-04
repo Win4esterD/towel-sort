1d42ee42cbacd5729842c5887b6ce15235bc31b2
Pavel Didenko <win4ester2016@yandex.ru>
2022-08-05 00:58:19 +0300

    Resolved

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  sorted = [];
+  if (matrix == undefined)return []
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }
+  for (let i = 0; i < matrix.length; i++){
+    if(i == 0 || i%2 == 0){
+      matrix[i] = matrix[i].sort(compareNumeric);
+    }else if(i!=0 && i%2 != 0){
+      matrix[i] = matrix[i].sort(compareNumeric);
+      matrix[i] = matrix[i].reverse();
+    }
+  }
+    for(char of matrix){
+      for(item of char){
+        sorted.push(item);
+      }
+    }
+  return sorted;
+  }
